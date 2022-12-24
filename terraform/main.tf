ae2e2eab4d66042867c008d566067e00c08a302b
Pete Shima <me@peteshima.com>
2022-12-24 15:05:13 -0800

    Update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "pub1" {
   availability_zone = "us-west-2a"
 
   tags = {
-    Name = "${var.defaulttag}"
+    Name = "${var.defaulttag}-pub1"
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_subnet" "pub2" {
   availability_zone = "us-west-2b"
 
   tags = {
-    Name = "${var.defaulttag}"
+    Name = "${var.defaulttag}-pub2"
   }
 }
 
@@ -52,7 +52,7 @@ resource "aws_subnet" "priv1" {
   availability_zone = "us-west-2a"
 
   tags = {
-    Name = "${var.defaulttag}"
+    Name = "${var.defaulttag}-priv1"
   }
 }
 
@@ -62,7 +62,7 @@ resource "aws_subnet" "priv2" {
   availability_zone = "us-west-2b"
 
   tags = {
-    Name = "${var.defaulttag}"
+    Name = "${var.defaulttag}-priv2"
   }
 }
 
@@ -89,9 +89,9 @@ resource "aws_route_table_association" "pub2" {
   route_table_id = aws_route_table.main.id
 }
 
-resource "aws_security_group" "allow_ssh_https" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+resource "aws_security_group" "allow_external" {
+  name        = "dev-mastodon-main-allow"
+  description = "Allow inbound traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -100,6 +100,22 @@ resource "aws_security_group" "allow_ssh_https" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "3000 from VPC"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description = "4000 from VPC"
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
   ingress {
@@ -118,7 +134,48 @@ resource "aws_security_group" "allow_ssh_https" {
   }
 
   tags = {
-    Name = "${var.defaulttag}"
+    Name = "${var.defaulttag}-allow-external"
+  }
+}
+
+resource "aws_security_group" "allow_internal" {
+  name        = "dev-mastodon-main-allow-internal"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "3000 from VPC"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description = "4000 from VPC"
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.defaulttag}-allow-internal"
   }
 }
 
@@ -215,3 +272,48 @@ resource "aws_elasticache_cluster" "mastodon-redis" {
   security_group_ids = [aws_security_group.allow_redis.id]
   subnet_group_name   = "${aws_elasticache_subnet_group.main.name}"
 }
+
+resource "aws_lb" "dev-mastodon-main-alb" {
+  name               = "dev-mastodon-main-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_external.id]
+  subnets            = [aws_subnet.pub1.id, aws_subnet.pub2.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "${var.defaulttag}"
+  }
+}
+
+resource "aws_lb_target_group" "http-3000" {
+  name     = "dev-mastodon-main-alb-http-3000"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {    
+    healthy_threshold   = 3    
+    unhealthy_threshold = 3    
+    timeout             = 5    
+    interval            = 10    
+    path                = "/health"    
+    port                = "3000"  
+  }
+
+}
+
+resource "aws_lb_listener" "dev-mastodon-main-alb" {
+  load_balancer_arn = aws_lb.dev-mastodon-main-alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-west-2:235921289734:certificate/78d7154c-e7e7-4712-8e7b-ce1fb613e6e0"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.http-3000.arn
+  }
+}
+
